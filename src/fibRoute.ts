195a6d686a1be9fbb5473473ae598bf34aaa3aaa
Michelle <mliuu3259@gmail.com>
2023-01-23 12:25:37 -0500

    request and response types

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,16 @@
 // Endpoint for querying the fibonacci numbers
+import { Request, Response } from 'express';
+import fibonacci from './fib';
 
-const fibonacci = require("./fib");
+export default (req: Request, res: Response) => {
+    const { num } = req.params;
 
-export default (req, res) => {
-  const { num } = req.params;
+    const fibN: number = fibonacci(parseInt(num)) as number;
+    let result = `fibonacci(${num}) is ${fibN}`;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+    if (fibN < 0) {
+        result = `fibonacci(${num}) is undefined`;
+    }
 
-  if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
-  }
-
-  res.send(result);
+    res.send(result);
 };
